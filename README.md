5a38a9d7d21b6c8cdf78a9814cfc339a5de596cf
Vic <viccw02@gmail.com>
2020-11-03 15:27:19 -0500

    added python file

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 ## Author
 
 ## License
+MIT
---
+++ Variables_Python.py
@@ -0,0 +1,21 @@
+print("This is my first Python script! Exciting")
+
+name = "Trevor"
+age = "old"
+eyecolor = "blue"
+haircolor = "reddish - what's left, anyway"
+
+print("Name is currently" + name)
+
+name = input("What is YOUR name?")
+age = 22
+
+print("Your name is now:" + name + "and your age is" + str(age))
+
+counter = 0
+
+while (counter < 8): 
+	counter += 1
+	print("counter is now at:" + str(counter))
+
+print("counter is greater than 8, our loop is done")
